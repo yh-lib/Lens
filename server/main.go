a828bb67a5e90eb7eaec86d5479e0b9387fdb030
yaohui.li <liyaohuiisgreat@gmail.com>
2026-03-16 21:38:37 +0800

    添加后端服务脚手架并完成初始化

---
+++ server/main.go
@@ -0,0 +1,13 @@
+// 项目的总入口
+package main
+
+// func main() {
+// 	// 注册gin引擎
+// 	r := gin.Default()
+// 	// 使用中间件鉴权
+// 	r.Use(middlerwares.JwtAuth)
+// 	// 注册路由
+// 	routers.RegistrerRouters(r)
+// 	// 启动程序
+// 	r.Run(config.Port)
+// }
---
+++ server/routers/routers.go
@@ -0,0 +1,16 @@
+// 路由层 管理程序的路由信息
+package routers
+
+import (
+	"server/routers/auth"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 注册路由的方法
+func RegistrerRouters(r *gin.Engine) {
+	// 1. 登录: /api/auth/login
+	// 2. 退出: /api/auth/logout
+	apiGroup := r.Group("/api")
+	auth.RegisterSubRouter(apiGroup)
+}
