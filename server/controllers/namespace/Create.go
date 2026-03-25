47cc6fed0f5669e30724c9fd4d33613a68b16c54
yaohui.li <liyaohuiisgreat@gmail.com>
2026-03-26 02:23:36 +0800

    删除namespace功能已完成

--- server/controllers/namespace/Create.go
+++ server/controllers/namespace/Create.go
@@ -15,19 +15,19 @@ func Create(c *gin.Context) {
 	logs.Info(nil, "创建逻辑")
 	returnData := config.ReturnData{}
 	clientSet, basicInfo, err := controllers.BasicInit(c)
-	var newNameSpace corev1.Namespace
-	newNameSpace.Name = basicInfo.NameSpace
 	if err != nil {
 		logs.Error(map[string]any{"Error": err}, "clientSet 初始化失败")
 	}
+	var newNameSpace corev1.Namespace
+	newNameSpace.Name = basicInfo.NameSpace
 	_, err = clientSet.CoreV1().Namespaces().Create(context.TODO(), &newNameSpace, metav1.CreateOptions{})
 	if err != nil {
 		returnData.Status = 400
 		returnData.Message = "创建 namespace: " + newNameSpace.Name + " 失败:" + err.Error()
+		logs.Error(map[string]any{"Error": err}, "创建 namespace: "+newNameSpace.Name+" 失败:")
 	} else {
 		returnData.Status = 200
 		returnData.Message = "创建 namespace: " + newNameSpace.Name + " 成功:"
 	}
-	logs.Error(map[string]any{"Error": err}, "创建 namespace: "+newNameSpace.Name+" 失败:")
 	c.JSON(200, returnData)
 }
--- server/controllers/namespace/Delete.go
+++ server/controllers/namespace/Delete.go
@@ -1,11 +1,33 @@
 package namespace
 
 import (
+	"context"
+	"server/config"
+	"server/controllers"
 	"server/utils/logs"
 
 	"github.com/gin-gonic/gin"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Delete(c *gin.Context) {
 	logs.Info(nil, "删除逻辑")
+	returnData := config.ReturnData{}
+	clientSet, basicInfo, err := controllers.BasicInit(c)
+	if err != nil {
+		logs.Error(map[string]any{"Error": err}, "clientSet 初始化失败")
+	}
+	var nameSpace corev1.Namespace
+	nameSpace.Name = basicInfo.NameSpace
+	err = clientSet.CoreV1().Namespaces().Delete(context.TODO(), nameSpace.Name, metav1.DeleteOptions{})
+	if err != nil {
+		returnData.Status = 400
+		returnData.Message = "删除 namespace: " + nameSpace.Name + " 失败:" + err.Error()
+		logs.Error(map[string]any{"Error": err}, "删除 namespace: "+nameSpace.Name+" 失败:")
+	} else {
+		returnData.Status = 200
+		returnData.Message = "删除 namespace: " + nameSpace.Name + " 成功:"
+	}
+	c.JSON(200, returnData)
 }
