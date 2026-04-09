ff8378164a6438eb77ff232c4c8947e0ab3cc911
yaohui.li <liyaohuiisgreat@gmail.com>
2026-04-09 10:38:36 +0800

    前端：创建deployment基本配置页面样式已完成

--- web/src/view/components/DialogByCreateItem.vue
+++ web/src/view/components/DialogByCreateItem.vue
@@ -1,10 +1,20 @@
 <script setup>
-import { ref } from 'vue'
+import { onBeforeMount, reactive, ref } from 'vue'
 import ElCard from './ElCard.vue';
 
 const props = defineProps(['openDialog'])
 const emit = defineEmits(['closeDialog'])
 const activeName = ref('Basic')
+const switchHostNetwork = ref(false)
+const switchAddService = ref(false)
+const data = reactive({
+  privateRepoSecret:[],
+})
+
+onBeforeMount(()=>{
+  // 获取私有仓库密钥，并赋值给data.privateRepoSecret
+  
+})
 
 </script>
 
@@ -13,7 +23,7 @@ const activeName = ref('Basic')
   <el-dialog
     v-model="props.openDialog"
     width="1600px"
-    style="height: 840px;"
+    style="height: 700px;"
     @close="emit('closeDialog')"
   >
     <el-tabs v-model="activeName" @tab-click="handleClick">
@@ -21,11 +31,76 @@ const activeName = ref('Basic')
         <el-tab-pane label="基本配置" name="Basic">
             <ElCard :op-cluster="true" :op-ns="true" style="border-radius: 0px;width: 1560px;">
               <template #mainData>
-                <el-row :gutter="20">
-                  <el-col :span="8"><div class="grid-content ep-bg-purple" />名称</el-col>
-                  <el-col :span="8"><div class="grid-content ep-bg-purple" />副本数</el-col>
-                  <el-col :span="8"><div class="grid-content ep-bg-purple" />私有仓库密钥</el-col>
-                </el-row>
+                <el-form label-width="150px" label-position="left" style="height: 450px;width: 1460px;">
+                  <el-row :gutter="100">
+                    <!-- 第一行 -->
+                    <el-col :span="8" class="form-item">
+                      <el-form-item label="名称" required>
+                        <el-input placeholder="请输入资源名称"/>
+                      </el-form-item>
+                    </el-col>
+                    <el-col :span="8" class="form-item">
+                      <el-form-item label="副本数" >
+                        <el-input placeholder="请输入副本数"/>
+                      </el-form-item>
+                    </el-col>
+                    <el-col :span="8" class="form-item">
+                      <el-form-item label="私有仓库密钥">
+                        <el-select placeholder="请选择私有仓库密钥">
+                          <el-options 
+                          
+                          />
+                        </el-select>
+                      </el-form-item>
+                    </el-col>
+                    <!-- 第二行 -->
+                    <el-col :span="8" class="form-item">
+                      <el-form-item label="DNS 策略">
+                        <el-select placeholder="请选择 DNS 策略">
+                          <el-options 
+                          
+                          />
+                        </el-select>
+                      </el-form-item>
+                    </el-col>
+                    <el-col :span="8" class="form-item">
+                      <el-form-item label="更新策略">
+                        <el-select placeholder="请选择更新策略">
+                          <el-options 
+                          
+                          />
+                        </el-select>
+                      </el-form-item>
+                    </el-col>
+                    <el-col :span="8" class="form-item">
+                      <div style="display: flex;justify-content: space-between;">
+                        <el-form-item label="最大不可用" label-width="100px">
+                          <el-input placeholder="" style="width: 100px"/>
+                        </el-form-item>
+                        <el-form-item label="可超出最大值" label-width="100px">
+                          <el-input placeholder="" style="width: 100px"/>
+                        </el-form-item>
+                      </div>
+                      
+                    </el-col>
+                    <!-- 第三行 -->
+                    <el-col :span="8" class="form-item">
+                      <el-form-item label="使用宿主机网络">
+                        <el-switch
+                          v-model="switchHostNetwork"
+                          style="--el-switch-on-color: #13ce66;"
+                        />
+                      </el-form-item>
+                    </el-col>
+                    <el-col :span="8" class="form-item">
+                      <el-form-item label="自动添加 Servcie">
+                        <el-switch
+                          v-model="switchAddService"
+                        />
+                      </el-form-item>
+                    </el-col>
+                  </el-row>
+                </el-form>
               </template>                
             </ElCard>
         </el-tab-pane>
@@ -37,4 +112,9 @@ const activeName = ref('Basic')
   </el-dialog>
 </template>
 
+<style scoped>
+.form-item{
+  margin-bottom: 15px;
+}
+</style>
 
